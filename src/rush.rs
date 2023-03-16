231a6163a1ba0df54ed205744c6fd64647062617
Jack Darlison <jdarlison7@gmail.com>
2023-03-16 21:26:12 +0000

    Programatic debugging setup

--- src/rush.rs
+++ src/rush.rs
@@ -1,4 +1,5 @@
-use log::info;
+use log::{info, LevelFilter};
+use log4rs::{append::file::FileAppender, encode::pattern::PatternEncoder, Config, config::{Appender, Root}};
 
 use crate::interface::engine;
 
@@ -13,7 +14,17 @@ mod interface;
 
 fn main() {
 
-    log4rs::init_file("log_config.yaml", Default::default()).unwrap();
+    let file = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
+        .build("log/rush.log")
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("file", Box::new(file)))
+        .build(Root::builder().appender("file").build(LevelFilter::Debug))
+        .unwrap();
+
+    let _handle = log4rs::init_config(config).unwrap();
 
     info!("Rush Initiated");
 
